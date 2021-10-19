ae1c7f6b7ff154b8d874a8743939678139362f2e
Miguel Felipe <30815011+KaelGeass@users.noreply.github.com>
2021-10-19 10:15:37 -0300

    Add files via upload

---
+++ Assets/Demo/Scripts(Demo)/AdaptiveAudioSceneManager.cs
@@ -0,0 +1,39 @@
+﻿using AdaptiveAudio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdaptiveAudioSceneManager : MonoBehaviour
+{
+    public GameState gameState;
+
+    VerticalAudioManager AudioManager;
+
+    void Start()
+    {
+        AudioManager = VerticalAudioManager.Instance;
+        gameState = GameState.Intro;
+    }
+
+    void Update()
+    {
+        switch (gameState)
+        {
+            case GameState.Intro:
+                break;
+            case GameState.Idle:
+                break;
+            case GameState.LightCombat:
+                break;
+            case GameState.HardCombat:
+                break;
+            case GameState.Ending:
+                break;
+        }
+    }
+
+    public enum GameState
+    {
+        Intro, Idle, LightCombat, HardCombat, Ending
+    }
+}
---
+++ Assets/Demo/Scripts(Demo)/CharacterController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float maxSpeed = 6f;
+    float horizontalSpeed = 0f;
+    float verticalSpeed = 0f;
+    Animator animator;
+    Rigidbody2D body;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update(){
+
+        SetAnimation();
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            body.velocity = new Vector2(-maxSpeed, body.velocity.y);
+            MovePlayer(horizontalSpeed, verticalSpeed);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            body.velocity = new Vector2(maxSpeed, body.velocity.y);
+            MovePlayer(horizontalSpeed, verticalSpeed);
+        }
+        else {
+            body.velocity = new Vector2(0, body.velocity.y);
+            MovePlayer(horizontalSpeed, verticalSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            body.velocity = new Vector2(body.velocity.x, -maxSpeed);
+            MovePlayer(horizontalSpeed, verticalSpeed);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow)) {
+            body.velocity = new Vector2(body.velocity.x, maxSpeed);
+            MovePlayer(horizontalSpeed, verticalSpeed);
+        }
+        else {
+            body.velocity = new Vector2(body.velocity.x, 0);
+            MovePlayer(horizontalSpeed, verticalSpeed);
+        }
+    }
+
+    private void SetAnimation()
+    { 
+        if(body.velocity.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (body.velocity.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else{ }
+
+        animator.SetFloat("Velocity", body.velocity.sqrMagnitude);
+    }
+
+    void MovePlayer(float horizontalSpeed, float verticalSpeed)
+    {
+        body.AddForce(new Vector2(horizontalSpeed, verticalSpeed));
+    }
+}
